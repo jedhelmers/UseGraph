e7eeb961bd3de9e2d0fb0eb167e1470c0a787a2f
Jed Helmers <jedhelmers@gmail.com>
2024-05-28 19:56:18 -0400

    stuff

--- src/mdForm.jsx
+++ src/mdForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { ReactComponent as TrashCan} from './assets/trash-can.svg';
+import { ReactComponent as Link} from './assets/link.svg';
 import { ReactComponent as DownAlt} from './assets/level-down-alt.svg';
 import { ReactComponent as SquarePlus} from './assets/square-plus.svg';
 import { ReactComponent as SquarePlusFill} from './assets/square-plus-fill.svg';
@@ -15,6 +16,13 @@ const TYPES = [
     'String'
 ]
 
+const LinkBox = () => {
+
+    return (
+        <div className='link-box'></div>
+    )
+}
+
 
 const MetadataForm = ({
     metadata,
@@ -29,6 +37,7 @@ const MetadataForm = ({
     errorHandler
 }) => {
     const [formData, setFormData] = useState({ ...metadata });
+    const [showLinkBox, setShowLinkBox] = useState(false)
     const cardRef = useRef(null)
     const keynameRef = useRef(null)
 
@@ -83,104 +92,141 @@ const MetadataForm = ({
 
     return (
         <div ref={cardRef} className='metadata-item card card-body card-ht metadata-card child-card mb-3'>
-            <div className='space-between'>
-                {
-                    !isRoot &&
-                    <>
-                        <DownAlt
-                            style={{ width: 10, cursor: "pointer" }}
-                            disabled={isLocked}
-                            onClick={() => handleNodeSelect(id)}
-                        />
-                    </>
-                }
-                {
-                    isRoot &&
-                        <div className='row space-between full-width'>
-                            <div></div>
-                            <SquarePlusFill
-                                style={{ width: 20, cursor: "pointer" }}
+            <div className='card-item'>
+                {/* Left */}
+                <div className='space-between reverse-v'>
+                    {
+                        !isRoot &&
+                            <SquarePlus
+                                style={{ width: 20, cursor: "pointer", fill: 'white' }}
                                 disabled={isLocked}
                                 onClick={() => addChild(parentId)}
                             />
-                        </div>
-                }
-                <TrashCan
-                    style={{ width: 16, cursor: "pointer", fill: isRoot ? 'inherit' : 'white' }}
-                    disabled={isLocked} onClick={() => {
-                        handleNodeSelect(parentId)
-                        removeNode(id)
-                    }}
-                />
-            </div>
-            <div className='form-group col' ref={keynameRef}>
-                <label htmlFor='metadata-row-key' className='font-weight-bold'>Keyname:</label>
-                <input
-                    className='form-control form-control-ht'
-                    type="text"
-                    name="keyname"
-                    id="metadata-row-key"
-                    value={metadata.keyname}
-                    onChange={handleChange}
-                />
-                <div className='metadata-row-value-key-error form-group col alert-danger rounded p-3 hidden'>
-                    Empty keys are not allowed.
+                    }
                 </div>
-            </div>
-            <div className='form-group col'>
-                <label>Value:</label>
-                <input
-                    className='form-control form-control-ht'
-                    type="text"
-                    name="value"
-                    value={metadata.value}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className='form-group col'>
-                <label>Type:</label>
-                <select
-                    className='form-control form-control-ht'
-                    name="type"
-                    value={metadata.type}
-                    onChange={handleChange}
-                >
-                    {TYPES.map(type => (
-                        <option key={type} value={type}>
-                            {type}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className='form-group col'>
-                <label>Units:</label>
-                <input
-                    className='form-control form-control-ht'
-                    type="text"
-                    name="units"
-                    value={metadata.units}
-                    onChange={handleChange}
-                />
-            </div>
-            <div className='form-group col'>
-                <label>Annotation:</label>
-                <input
-                    className='form-control form-control-ht'
-                    type="text"
-                    name="annotation"
-                    value={metadata.annotation}
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                {
-                    !isRoot &&
-                        <SquarePlus
-                            style={{ width: 20, cursor: "pointer", fill: 'white' }}
-                            disabled={isLocked}
-                            onClick={() => addChild(parentId)}
+
+                {/* Middle */}
+                <div>
+                    <div className='full-width space-between'>
+                        <div></div>
+                        {
+                            isRoot &&
+                                <div className=''>
+                                    <div></div>
+                                    <SquarePlusFill
+                                        style={{ width: 20, cursor: "pointer" }}
+                                        disabled={isLocked}
+                                        onClick={() => addChild(parentId)}
+                                    />
+                                </div>
+                        }
+                        {
+                            !isRoot &&
+                            <>
+                                <DownAlt
+                                    style={{ width: 10, cursor: "pointer" }}
+                                    disabled={isLocked}
+                                    onClick={() => handleNodeSelect(id)}
+                                />
+                            </>
+                        }
+                        <div></div>
+                    </div>
+
+                    <div className='form-group col' ref={keynameRef}>
+                        <label htmlFor='metadata-row-key' className='font-weight-bold'>Keyname:</label>
+                        <input
+                            className='form-control form-control-ht'
+                            type="text"
+                            name="keyname"
+                            id="metadata-row-key"
+                            value={metadata.keyname}
+                            onChange={handleChange}
                         />
-                }
+                        <div className='metadata-row-value-key-error form-group col alert-danger rounded p-3 hidden'>
+                            Empty keys are not allowed.
+                        </div>
+                    </div>
+                    <div className='form-group col'>
+                        <label>Value:</label>
+                        <input
+                            className='form-control form-control-ht'
+                            type="text"
+                            name="value"
+                            value={metadata.value}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='form-group col'>
+                        <label>Type:</label>
+                        <select
+                            className='form-control form-control-ht'
+                            name="type"
+                            value={metadata.type}
+                            onChange={handleChange}
+                        >
+                            {TYPES.map(type => (
+                                <option key={type} value={type}>
+                                    {type}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    {
+                        showLinkBox && (
+                            <LinkBox />
+                        )
+                    }
+                    <div className='form-group col'>
+                        <label>Units:</label>
+                        <input
+                            className='form-control form-control-ht'
+                            type="text"
+                            name="units"
+                            value={metadata.units}
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className='form-group col'>
+                        <label>Annotation:</label>
+                        <input
+                            className='form-control form-control-ht'
+                            type="text"
+                            name="annotation"
+                            value={metadata.annotation}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                </div>
+
+                {/* Right */}
+                <div>
+                    <div className='space-between vertical full-height'>
+                        <div>
+                            <TrashCan
+                                style={{ width: 16, cursor: "pointer", fill: isRoot ? 'inherit' : 'white' }}
+                                disabled={isLocked}
+                                onClick={() => {
+                                    handleNodeSelect(parentId)
+                                    removeNode(id)
+                                }}
+                            />
+                            <input type='checkbox'/>
+                        </div>
+                        <Link
+                            style={{ width: 20, cursor: "pointer", fill: isRoot ? 'inherit' : 'white' }}
+                            disabled={isLocked}
+                            onClick={(e) => {
+                                e.target.value = 'Link'
+                                e.target.name = 'type'
+                                handleChange(e)
+                                setShowLinkBox(!showLinkBox)
+                            }}
+                        />
+                        <div></div>
+                    </div>
+                </div>
             </div>
         </div>
     );
