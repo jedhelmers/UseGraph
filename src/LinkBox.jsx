9f81b8ec02c05a0003aef318b4d7aa0668622432
jhelmers <jhelmers@miletwo.us>
2024-05-30 14:03:04 -0400

    tree + css

--- src/LinkBox.jsx
+++ src/LinkBox.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { WorkspaceTree } from './WorkspaceTree.jsx'
 
+const WORKSPACES = [{
+    name: "Butts",
+    id: 0,
+    children: [
+        {
+            name: 'Child of Butts',
+            id: 1,
+            children: [{
+                name: "Butts",
+                id: 2,
+                children: [
+                    {
+                        name: 'Child of Butts',
+                        id: 3
+                    }
+                ]
+            }]
+        }
+    ]
+}]
 
 const LinkBox = () => {
+    const [currentNode, setCurrentNode] = useState({id: 0})
+
+    useEffect(() => {
+        console.log('currentNode', currentNode)
+    }, [currentNode])
 
     return (
         <div className='link-box'>
             <div className='body'>
-                <div className='tree'>hi</div>
+                <div className='tree'>
+                    <WorkspaceTree data={WORKSPACES} setCurrentNode={setCurrentNode} currentNode={currentNode}/>
+                </div>
                 <div className='content'>ho</div>
             </div>
             <div className='buttons'>
---
+++ src/WorkspaceTree.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { ReactComponent as CaretDown } from './assets/caret-down.svg';
+import { ReactComponent as CaretRight } from './assets/caret-right.svg';
+
+const TreeNode = ({ node, errors, parentChain, isLocked, setCurrentNode, currentNode }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+    const hasChildren = node.children && node.children.length > 0;
+
+    const toggleExpand = (e) => {
+        e.stopPropagation();
+        setIsExpanded(!isExpanded);
+    };
+
+    const handleNodeSelect = (e) => {
+        e.stopPropagation();
+        setCurrentNode(node.id);
+    };
+
+    const selectionType = () => {
+        let output = '';
+
+        if (node?.id === currentNode?.id) {
+            output = 'selected';
+        }
+
+        if (parentChain.includes(node?.id)) {
+            output = 'parent-selected';
+        }
+
+        const regex = new RegExp(`.*-${currentNode?.id}$`, 'g');
+
+        if (errors.findIndex(error => error.match(regex)) > -1) {
+            output += " error";
+        }
+
+        return output;
+    };
+
+    return (
+        <li>
+            <div className={['row', selectionType()].join(' ')} id={node.id}>
+                <button disabled={isLocked} onClick={toggleExpand} className='expand'>
+                    {hasChildren ? (
+                        isExpanded ?
+                        <CaretDown style={{ width: 8, fill: 'rgba(255, 255, 255, .78)' }}/> :
+                        <CaretRight style={{ width: 8, fill: 'rgba(255, 255, 255, .78)' }}/>
+                    ) : ''}
+                </button>
+                <button disabled={isLocked} onClick={handleNodeSelect} className='navigate'>
+                    {node.name ? node.name : 'ERROR'}
+                </button>
+            </div>
+            {hasChildren && isExpanded && (
+                <ul className='collapsible'>
+                    {node.children.map(child => (
+                        <TreeNode
+                            key={child.id}
+                            node={child}
+                            errors={errors}
+                            parentChain={parentChain}
+                            isLocked={isLocked}
+                            setCurrentNode={setCurrentNode}
+                            currentNode={currentNode}
+                        />
+                    ))}
+                </ul>
+            )}
+        </li>
+    );
+};
+
+const Tree = ({ data, parentChain, errors, isLocked, setCurrentNode, currentNode }) => {
+    return (
+        <ul>
+            {data.map(node => (
+                <TreeNode
+                    key={node.id}
+                    node={node}
+                    errors={errors}
+                    parentChain={parentChain}
+                    isLocked={isLocked}
+                    setCurrentNode={setCurrentNode}
+                    currentNode={currentNode}
+                />
+            ))}
+        </ul>
+    );
+};
+
+const WorkspaceTree = ({ currentNode, rootId = 0, isLocked = false, errors = [], data = [], setCurrentNode = console.log, parentChain = [] }) => {
+    return (
+        <div className='navigation-tree'>
+            {data ? <Tree errors={errors} parentChain={parentChain} isLocked={isLocked} setCurrentNode={setCurrentNode} data={data} currentNode={currentNode} /> : <p>Loading tree...</p>}
+        </div>
+    );
+};
+
+export {
+    WorkspaceTree, Tree, TreeNode
+};
