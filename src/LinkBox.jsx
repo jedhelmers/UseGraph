5dd8ef02b4617e1c52fc4b22583eba396c073109
jhelmers <jhelmers@miletwo.us>
2024-05-30 11:04:58 -0400

    Link Box - init

--- src/LinkBox.jsx
+++ src/LinkBox.jsx
@@ -4,7 +4,16 @@ import React from 'react'
 const LinkBox = () => {
 
     return (
-        <div className='link-box'></div>
+        <div className='link-box'>
+            <div className='body'>
+                <div className='tree'>hi</div>
+                <div className='content'>ho</div>
+            </div>
+            <div className='buttons'>
+                <button className='btn btn-ht-secondary fmb-cancel'>Cancel</button>
+                <button className='btn btn-ht-primary fmb-select'>Select</button>
+            </div>
+        </div>
     )
 }
 
